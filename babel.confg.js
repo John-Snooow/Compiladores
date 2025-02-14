a1c5356393af17b83da752b35fb9549ac173b216
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-02-14 20:02:00 -0300

    Configurando targets

--- babel.confg.js
+++ babel.confg.js
@@ -1,3 +1,17 @@
-const presets = ["babel/preset-env"]// Esse preset permite que você escreva código JavaScript moderno (como ES6+) e o transpile automaticamente para uma versão compatível com navegadores mais antigos, com base nas configurações fornecidas.
+// Esse preset permite que você escreva código JavaScript moderno (como ES6+) e o transpile automaticamente para uma versão compatível com navegadores mais antigos, com base nas configurações fornecidas.
+const presets = [
+  [
+    "babel/preset-env",
+    {
+      // posso colocar objetos para vesões de navegadores
+      targets: {
+        edge: "17", // versão do navegador
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+      },
+    },
+  ],
+];
 
-module.exports= {presets} // para poder exportar
+module.exports = { presets }; // para poder exportar
