22ee0e886e28b090c54a1db3f87e58c8e97942c3
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-02-13 23:19:27 -0300

    Instalando e configurando o babel

---
+++ babel.confg.js
@@ -0,0 +1,3 @@
+const presets = ["babel/preset-env"]// Esse preset permite que você escreva código JavaScript moderno (como ES6+) e o transpile automaticamente para uma versão compatível com navegadores mais antigos, com base nas configurações fornecidas.
+
+module.exports= {presets} // para poder exportar
